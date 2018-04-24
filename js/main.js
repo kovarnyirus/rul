9d7805936ccaf876d56bbcbf8eb18d1c86fd4a8b
kovarnyirus <kovarnyi@gmail.com>
2018-04-24 18:00:25 +0300

    add slic slider

--- js/main.js
+++ js/main.js
@@ -1,34 +1,41 @@
-/*Мобильное меню*/
-var navMain = document.querySelector(".main-nav");
-var navToggle = document.querySelector(".main-nav-toggle");
-
-navMain.classList.remove('main-nav--nojs');
-
-navToggle.addEventListener("click", function() {
-  if (navMain.classList.contains("main-nav--closed")) {
-    navMain.classList.remove("main-nav--closed");
-    navMain.classList.add("main-nav--opened");
-  } else {
-    navMain.classList.add("main-nav--closed");
-    navMain.classList.remove("main-nav--opened");
-  }
-});
-
-var popap = document.querySelector('.popap');
-var popapOverlay = document.querySelector(".popap-overlay");
-var buy = document.querySelector("#buy");
-
-buy.addEventListener("click", function() {
-    popap.classList.remove("popap--closed");
-    popap.classList.add("popap--opened");
-    popapOverlay.classList.remove("popap--closed");
-});
-window.addEventListener("keydown", function(event) {
-  if (event.keyCode === 27) {
-    if (popap.classList.contains("popap--opened")) {
-      popap.classList.remove("popap--opened");
-      popap.classList.add("popap--closed");
-      popapOverlay.classList.add("popap--closed");
-    }
-  }
+// /*Мобильное меню*/
+// var navMain = document.querySelector(".main-nav");
+// var navToggle = document.querySelector(".main-nav-toggle");
+//
+// navMain.classList.remove('main-nav--nojs');
+//
+// navToggle.addEventListener("click", function() {
+//   if (navMain.classList.contains("main-nav--closed")) {
+//     navMain.classList.remove("main-nav--closed");
+//     navMain.classList.add("main-nav--opened");
+//   } else {
+//     navMain.classList.add("main-nav--closed");
+//     navMain.classList.remove("main-nav--opened");
+//   }
+// });
+//
+// var popap = document.querySelector('.popap');
+// var popapOverlay = document.querySelector(".popap-overlay");
+// var buy = document.querySelector("#buy");
+//
+// buy.addEventListener("click", function() {
+//     popap.classList.remove("popap--closed");
+//     popap.classList.add("popap--opened");
+//     popapOverlay.classList.remove("popap--closed");
+// });
+// window.addEventListener("keydown", function(event) {
+//   if (event.keyCode === 27) {
+//     if (popap.classList.contains("popap--opened")) {
+//       popap.classList.remove("popap--opened");
+//       popap.classList.add("popap--closed");
+//       popapOverlay.classList.add("popap--closed");
+//     }
+//   }
+// });
+$('.slick-slider').slick({
+  infinite: true,
+  dots: false,
+  arrows: false,
+  slidesToShow: 4,
+  slidesToScroll: 1
 });
